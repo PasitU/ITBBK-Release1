d1042195ccbc7d08546d860814a5738a1fbf67f4
PasitU <111435313+PasitU@users.noreply.github.com>
2024-04-26 22:50:20 +0700

    error modal & loading

--- itbkk-fe/src/api/taskService.js
+++ itbkk-fe/src/api/taskService.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-useless-catch */
 // const BASE_URL = 'http://ip23us2.sit.kmutt.ac.th:8080/itb-kk/v1/tasks'
-const BASE_URL = 'http://localhost:8080/v1/tasks'
+const BASE_URL = 'http://localhost:8080/itb-kk/v1/tasks'
 // const BASE_URL = 'http://localhost:3000/tasks'
 
 export const getAllTasks = async () => {
