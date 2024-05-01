ff6998e0fb9615525a57ee3c342638733c22fc64
NTPLSRPP <nathaphat.n07@gmail.com>
2024-05-01 16:39:37 +0700

    Refactor timeConverter.js to use toLocaleString for date formatting

--- itbkk-fe/src/utils/timeConverter.js
+++ itbkk-fe/src/utils/timeConverter.js
@@ -9,27 +9,10 @@ function getUserTimeZoneLocale() {
 }
 
 function UTCtoLocalFormat(utcTimeString) {
-  let utcDate
-  utcDate = new Date(utcTimeString)
-
-  const offsetMinutes = utcDate.getTimezoneOffset()
-
-  const localDate = new Date(utcDate.getTime() - offsetMinutes * 60000)
-  const day = String(localDate.getDate()).padStart(2, '0')
-  const month = String(localDate.getMonth() + 1).padStart(2, '0')
-  const year = localDate.getFullYear()
-  const hours = String(localDate.getHours()).padStart(2, '0')
-  const minutes = String(localDate.getMinutes()).padStart(2, '0')
-  const seconds = String(localDate.getSeconds()).padStart(2, '0')
-
-  const formattedDate = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`
-
+  let utcDate = new Date(utcTimeString)
+  console.log(utcDate);
+  const formattedDate = utcDate.toLocaleString('en-GB')
   return formattedDate
 }
-
-// Example usage:
-// console.log(getUserTimeZoneId())
-// console.log(UTCtoLocalFormat('2024-04-22T09:30:00'))
-// console.log(UTCtoLocalFormat('2024-04-22T09:00:00Z'))
 
 export { getUserTimeZoneId, getUserTimeZoneLocale, UTCtoLocalFormat }
